c1026154ab6d625dd8c576c3146ea6a7229ca611
Gustavo Domingos <gutogutao2010@gmail.com>
2025-09-09 08:41:29 -0300

    Contagem_Regressiva

---
+++ contagem_regressiva.py
@@ -0,0 +1,8 @@
+import time
+print('========================')
+print('   CONTAGEM REGRESSIVA  ')
+print('========================')
+for i in range(10,0,-1):
+    print(f"{i}")
+    time.sleep(1)
+print('BOOOM')
